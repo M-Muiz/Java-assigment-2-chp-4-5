1951a92a6f2aa96e4db4ad525069988a68a4bcbb
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-01-28 15:38:28 +0500

    java-2

---
+++ Assigment 16/java/index.js
@@ -0,0 +1,54 @@
+// Q:1
+
+alert ("Moiz");
+
+
+//Q:2
+
+var a = ("16 years old");
+
+alert (a);    
+
+
+//Q:3
+
+var a =("message");
+
+a = ("Hello World");
+
+alert (a);
+
+//Q:4
+
+var b = ("Certified mobile appliacation developer");
+
+alert (b);
+
+//Q:5
+
+var c = ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+alert (c);
+
+//Q:6;
+
+var d = ("my email address is example@gmail.com");
+
+alert (d);
+
+//Q:7
+var e = ("I am trying to learn form book  A smarter\n way to learn javascript");
+
+alert(e);
+
+//Q:8
+
+document.write ("<h1>Yah! i can write HTML content through my java script");
+
+//Q:9
+
+var f = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+alert (f);
+
+document.write ("<center>__END__ </center>");
